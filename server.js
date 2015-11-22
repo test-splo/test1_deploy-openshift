0d82d1759f59d0d9f449165679d8a4b8d9f5ac2c
SpinyLobster <spinylobster@outlook.com>
2015-11-22 17:23:03 +0900

    test

---
+++ server.js
@@ -0,0 +1,30 @@
+var cc          = require('config-multipaas'),
+    restify     = require('restify'),
+    fs          = require('fs')
+
+var config      = cc(),
+    app         = restify.createServer()
+
+app.use(restify.queryParser())
+app.use(restify.CORS())
+app.use(restify.fullResponse())
+
+// Routes
+app.get('/status', function (req, res, next)
+{
+  res.send("{status: 'ok'}");
+});
+
+app.get('/', function (req, res, next)
+{
+  var data = fs.readFileSync(__dirname + '/index.html');
+  res.status(200);
+  res.header('Content-Type', 'text/html');
+  res.end(data.toString().replace(/host:port/g, req.header('Host')));
+});
+
+app.get(/\/(css|js|img)\/?.*/, restify.serveStatic({directory: './static/'}));
+
+app.listen(config.get('PORT'), config.get('IP'), function () {
+  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+});
